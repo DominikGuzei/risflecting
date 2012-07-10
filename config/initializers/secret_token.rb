f505e0b2b84d3fd00ccd34a7c95cee1e82724012
David Strauß <mail@stravid.com>
2012-07-10 19:47:45 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Risflecting::Application.config.secret_token = '94c450a303ca6423f8d70e50397694eec7900777ebde6d68641476d3d3af07950e13e4279495b99fe88b8cdcd18be3b47f9179deac8bb22b2c097078f6324cfe'
