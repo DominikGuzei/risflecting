ce30329c6353670edac65abedd857f1e34f93258
David Strauß <mail@stravid.com>
2012-07-21 11:20:29 +0200

    Allow nested static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Risflecting::Application.routes.draw do
-  match '/:id' => 'high_voltage/pages#show', :as => :static, :via => :get
+  match '/*id' => 'high_voltage/pages#show', :as => :static, :via => :get
 
   root :to => 'high_voltage/pages#show', :id => 'willkommen'
 end
