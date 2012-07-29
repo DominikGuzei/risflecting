e9575ad19fc1ca6fe22aa1ffc59d75722590c973
Dominik Guzei <dominik.guzei@gmail.com>
2012-07-29 23:11:28 +0200

    WIP responsive design with bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  
+  def page_title(title)
+    content_for :page_title do title end
+  end
+  
+  def page_stylesheet(path, options)
+    content_for :stylesheet_includes do
+      stylesheet_link_tag path, options
+    end
+  end
+  
+  def page_javascript(path)
+    content_for :javascript_includes do
+      javascript_include_tag path
+    end
+  end
+  
 end
