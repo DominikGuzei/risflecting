0a6562c8cc1d7671db144a00ed22eb0baadee7b0
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-03 08:10:02 +0200

    Introduces configuration file

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,10 @@ module Risflecting
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.before_configuration do
+      require File.expand_path('../configure', __FILE__)
+    end
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
