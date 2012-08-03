3483b455ca5a4d33ce3c8fb146f6e9a096afd4ad
David Strauß <mail@stravid.com>
2012-08-03 09:22:57 +0200

    Adds the namespace for the internal website and a dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Risflecting::Application.routes.draw do
+
+  namespace :intern do
+    resource :dashboard, :only => [:show]
+  end
+
+  match '/intern' => 'intern/dashboards#show'
+
   match '/*id' => 'high_voltage/pages#show', :as => :static, :via => :get
 
   root :to => 'high_voltage/pages#show', :id => 'willkommen'
