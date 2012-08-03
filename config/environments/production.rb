de561a676f8b2a6e58838b9089374eaa5b1ee84a
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-03 09:25:22 +0200

    Renames all .scss files to end with .css.scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Risflecting::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( 'pages/willkommen.scss', 'pages/konzept.scss' )
+  # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
