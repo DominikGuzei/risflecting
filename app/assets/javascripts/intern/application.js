eda304915146ab62ed245f128ca7c49a6a1a03bf
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-06 17:52:32 +0200

    Moves external js and css files to 'vendor'

--- app/assets/javascripts/intern/application.js
+++ app/assets/javascripts/intern/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require ./bootstrap_datepicker
-//= require ./bootstrap_datepicker_de
+//= require bootstrap_datepicker
+//= require bootstrap_datepicker_de
 //= require_tree .
