99f88f818d96801921f998b1b0c1a04b82da76fe
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-07 07:41:42 +0200

    Adds route specs

--- spec/routing/intern_routes_spec.rb
+++ spec/routing/intern_routes_spec.rb
@@ -7,4 +7,25 @@ describe 'Intern' do
       :action => 'show'
     )
   end
+
+  it 'should route GET /intern/appointments to the appointment list' do
+    { :get => '/intern/appointments' }.should route_to(
+      :controller => 'intern/appointments',
+      :action => 'index'
+    )
+  end
+
+  it 'should route GET /intern/appointments/new to the appointment creation form' do
+    { :get => '/intern/appointments/new' }.should route_to(
+      :controller => 'intern/appointments',
+      :action => 'new'
+    )
+  end
+
+  it 'should route POST /intern/appointments to the appointment create action' do
+    { :post => '/intern/appointments' }.should route_to(
+      :controller => 'intern/appointments',
+      :action => 'create'
+    )
+  end
 end
