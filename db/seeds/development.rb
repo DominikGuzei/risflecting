e27ea4a6a1de568159c9d7108e18aca90ba3bceb
David Strauß <mail@stravid.com>
2012-08-07 21:21:25 +0200

    Add seed file for development environment

---
+++ db/seeds/development.rb
@@ -0,0 +1,11 @@
+unless User.exists? :email => 'admin@risflecting.dev'
+  user = User.new :email => 'admin@risflecting.dev',
+                  :forename => 'Admin',
+                  :surname => 'Admin',
+                  :phone => '042',
+                  :password => 'password',
+                  :password_confirmation => 'password'
+
+  user.confirmed_at = Time.now
+  user.save
+end
