27caef76b076981c218c8f97848c00a170570df0
David Strauß <mail@stravid.com>
2012-08-08 11:01:45 +0200

    Clean up User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :forename, :surname, :phone
-  # attr_accessible :title, :body
 
   validates :forename, :surname, :phone, :presence => true
 
-   def passwords_match?
+  def passwords_match?
     password == password_confirmation && !password.blank?
   end
 end
