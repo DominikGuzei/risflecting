12a545f509099afc2a21fb1ac0b3faad529ef689
David Strauß <mail@stravid.com>
2012-08-09 13:56:09 +0200

    Add Role model and association with the User model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+# General seeds which are the same for all environments
+Role.find_or_create_by_name 'Admin'
+Role.find_or_create_by_name 'Member'
+
+# Specific seeds
 file = File.join Rails.root, "db", "seeds", "#{Rails.env}.rb"
 
 load file if File.exist? file
