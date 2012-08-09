7fdb45c1d7451641ec7f6db1478a55bceb9725bf
David Strauß <mail@stravid.com>
2012-08-09 19:19:13 +0200

    Add missing specs for create action of AccountsController

--- spec/controllers/intern/accounts_controller_spec.rb
+++ spec/controllers/intern/accounts_controller_spec.rb
@@ -17,4 +17,22 @@ describe Intern::AccountsController do
       flash[:error].should_not be_nil
     end
   end
+
+  describe '#create' do
+    it 'should give admins access' do
+      Role.find_or_create_by_name 'Member'
+      sign_in FactoryGirl.create(:admin)
+      post :create, :user => { :email => 'test@test.com' }
+
+      response.should redirect_to new_intern_account_path
+    end
+
+    it 'should deny normal users access' do
+      sign_in FactoryGirl.create(:user)
+      post :create, :user => { :email => 'test@test.com' }
+
+      response.should redirect_to intern_root_url
+      flash[:error].should_not be_nil
+    end
+  end
 end
