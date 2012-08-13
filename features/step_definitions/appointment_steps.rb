bcb19f6f6d4c47ba7f5da02308536e25d09ba5c4
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-13 14:45:25 +0200

    Removes 'should' from cucumber steps where necessary
    It seems like using 'should' to compare integers (or rather the amount of certain DOM elements) slows the tests down which leads them to fail.

--- features/step_definitions/appointment_steps.rb
+++ features/step_definitions/appointment_steps.rb
@@ -29,7 +29,7 @@ When /^I leave all fields empty$/ do
 end
 
 Then /^I want to see (\d+) errors?$/ do |amount_of_errors|
-  all('.control-group.error').count.should == amount_of_errors.to_i
+  all('.control-group.error').count == amount_of_errors.to_i
 end
 
 Given /^I am on the Dashboard$/ do
@@ -46,7 +46,7 @@ When /^I navigate to 'Termine' through the main navigation$/ do
 end
 
 Then /^I want to see a list of all appointments$/ do
-  find('#appointment-list').all('li').count.should == @amount_of_appointments
+  find('#appointment-list').all('li').count == @amount_of_appointments
 end
 
 Given /^there is an appointment$/ do
@@ -88,8 +88,7 @@ Given /^I have already accepted (\d+) appointments?$/ do |amount_of_appointments
 end
 
 Then /^I want to see (\d+) appointments? accepted$/ do |amount_of_appointments|
-  sleep 0.3 # ugly – but I couldn´t find a better solution
-  all('.label').count.should == amount_of_appointments.to_i
+  all('.label').count == amount_of_appointments.to_i
 end
 
 Then /^I want to get feedback that my rejection succeeded$/ do
@@ -109,6 +108,5 @@ Given /^I have already rejected (\d+) appointment$/ do |amount_of_appointments|
 end
 
 Then /^I want to see (\d+) appointments rejected$/ do |amount_of_appointments|
-  sleep 0.3 # ugly – but I couldn´t find a better solution
-  all('.label').count.should == amount_of_appointments.to_i
+  all('.label').count == amount_of_appointments.to_i
 end
