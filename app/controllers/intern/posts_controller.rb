3fd7a1af3afab965224efdea7caf7701b741e0f8
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-14 19:51:09 +0200

    Implements the PostsController#create

--- app/controllers/intern/posts_controller.rb
+++ app/controllers/intern/posts_controller.rb
@@ -4,11 +4,20 @@ class Intern::PostsController < InternController
   end
 
   def show
+    @post = Post.find params[:id]
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_intern_user.posts.new params[:post]
+    if @post.save
+      redirect_to intern_post_path @post
+      flash[:success] = 'Deine Frage/Mitteilung wurde erfolgreich veröffentlicht'
+    else
+      render :new
+    end
   end
 end
