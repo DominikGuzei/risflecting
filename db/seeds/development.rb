f723f1124194955117c9fc625c6ed0e258978e48
Dominik Guzei <dominik.guzei@gmail.com>
2012-08-15 17:11:36 +0200

    fixes issue with seed data while creating admin user

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -2,12 +2,13 @@ unless User.exists? :email => 'admin@risflecting.dev'
   user = User.new :email => 'admin@risflecting.dev',
                   :forename => 'Admin',
                   :surname => 'Admin',
-                  :phone => '042',
+                  :phone => '+41 44 111 22 33',
                   :password => 'password',
                   :password_confirmation => 'password'
 
   user.roles << Role.find_by_name('Admin')
   user.confirmed_at = Time.now
 
-  user.save
+  user.save!
+
 end
