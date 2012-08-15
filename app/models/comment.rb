998de332c89dd5b243203175f92f8c361279c049
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-15 13:34:40 +0200

    Touches post for each new comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :post
+  belongs_to :post, :touch => true
   belongs_to :author, :class_name => 'User', :foreign_key => :user_id
 
   attr_accessible :text
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe Comment do
+  let(:post_instance) { FactoryGirl.create :post }
+
   it { should belong_to :author }
   it { should belong_to :post }
 
   it { should validate_presence_of :text }
+
+  it 'should touch the post on creation' do
+    original_updated_at = post_instance.updated_at
+
+    post_instance.comments.create :text => 'Some text'
+
+    last_post = Post.last
+    post_instance.should == last_post
+    original_updated_at.should_not == last_post.updated_at
+  end
 end
