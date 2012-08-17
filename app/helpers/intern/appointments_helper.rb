57a8d19abe7035ebb4faa53ff69453380432d30e
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-17 18:53:38 +0200

    Adds view helper to format date/time periods

--- app/helpers/intern/appointments_helper.rb
+++ app/helpers/intern/appointments_helper.rb
@@ -14,4 +14,17 @@ module Intern::AppointmentsHelper
       link_to label, reject_intern_appointment_path(options[:appointment]), :method => :post, :class => 'btn btn-danger'
     end
   end
+
+  def format_time_period start_time, end_time
+    if start_time.day == end_time.day
+      (
+        l(start_time, :format => '%d. %B %Y') +
+        '<div class="times">' + l(start_time, :format => '%H.%M') + ' - ' + l(end_time, :format => '%H.%M') + ' Uhr' + '</div>'
+      ).html_safe
+    elsif start_time.month == end_time.month
+      l(start_time, :format => '%d.') + ' - ' + l(end_time, :format => '%d. %B %Y')
+    else
+      l(start_time, :format => '%d. %b.') + ' - ' + l(end_time, :format => '%d. %b. %Y')
+    end
+  end
 end
