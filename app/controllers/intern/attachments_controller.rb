9ce4830e9883b0364c647b1a145c30a1a3a15fde
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-20 23:52:17 +0200

    Simplifies authorization for adding attachments to a question

--- app/controllers/intern/attachments_controller.rb
+++ app/controllers/intern/attachments_controller.rb
@@ -1,19 +1,15 @@
 class Intern::AttachmentsController < InternController
   def create
     @post = Post.find params[:post_id]
+    authorize! :add_attachment, @post
 
-    if can? :add_attachment, @post
-      @attachment = @post.attachments.new params[:attachment]
+    @attachment = @post.attachments.new params[:attachment]
 
-      if @attachment.save
-        redirect_to intern_post_path @post
-        flash[:success] = 'Deine Datei wurde erfolgreich hochgeladen'
-      else
-        render 'intern/posts/show'
-      end
+    if @attachment.save
+      redirect_to intern_post_path @post
+      flash[:success] = 'Deine Datei wurde erfolgreich hochgeladen'
     else
-      flash[:error] = 'Zugriff verweigert'
-      redirect_to intern_root_path
+      render 'intern/posts/show'
     end
   end
 end
