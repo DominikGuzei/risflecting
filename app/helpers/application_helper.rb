71ff3a7cce7fe77e95f0df4ccaac5eae45ecab8f
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-20 12:58:47 +0200

    Adds rinku (gem) for auto linking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-  
+
   def page_title(title)
     content_for :page_title do title end
   end
-  
+
   def page_stylesheet(path, options)
     content_for :stylesheet_includes do
       stylesheet_link_tag path, options
     end
   end
-  
+
   def page_javascript(path)
     content_for :javascript_includes do
       javascript_include_tag path
@@ -26,5 +26,4 @@ module ApplicationHelper
     end
     flash_messages.join("\n").html_safe
   end
-
 end
