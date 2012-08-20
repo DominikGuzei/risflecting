a2ca453c2750abc9d832dbb8ab7dbdfff5e6be18
David Strauß <mail@stravid.com>
2012-08-20 22:19:54 +0200

    Use different bucket for uploads from development

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                         # required
     :aws_access_key_id      => ENV['S3_ACCESS_KEY_ID'],       # required
     :aws_secret_access_key  => ENV['S3_SECRET_ACCESS_KEY'],   # required
-    :region                 => 'eu-west-1'                      # optional, defaults to 'us-east-1'
+    :region                 => 'eu-west-1'                    # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'risflecting'                       # required
+  config.fog_directory = 'risflecting'                        # required
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.storage = :fog
 end
@@ -14,5 +14,11 @@ if Rails.env.test? || Rails.env.cucumber?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
+  end
+end
+
+if Rails.env.development?
+  CarrierWave.configure do |config|
+    config.fog_directory = 'risflecting-development'
   end
 end
