8abf3b9889f1d72e8ac9b6cad1a88b5fb2e6aa4f
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-08-22 11:08:59 +0200

    Sets up view helper to parse file extension to a media category

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,15 +15,4 @@ module ApplicationHelper
       javascript_include_tag path
     end
   end
-
-  def bootstrap_flash_messages
-    flash_messages = []
-    flash.each do |type, message|
-      type = :info     if type == :notice
-      type = :warning  if type == :alert
-      text = content_tag(:div, message, :class => "flash-message alert alert-#{type.to_s}")
-      flash_messages << text if message
-    end
-    flash_messages.join("\n").html_safe
-  end
 end
