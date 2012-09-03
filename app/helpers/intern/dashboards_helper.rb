75e05cd0899959e61539c8083a83a4291b40be7a
Thomas Buchöster <thomas.buchoester@gmail.com>
2012-09-03 19:48:42 +0200

    Optimizes truncation of titles in dashboard

--- app/helpers/intern/dashboards_helper.rb
+++ app/helpers/intern/dashboards_helper.rb
@@ -1,6 +1,6 @@
 module Intern::DashboardsHelper
   def truncate_title title, time_text
-    available_length = 75 - time_text.size
+    available_length = 65 - time_text.size
     truncate title, :length => available_length, :separator => ' ', :omission => ' ...'
   end
 end
