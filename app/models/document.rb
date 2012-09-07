04ad548f0af7d5a602a07e4c9005d5b3e9aac353
David Strauß <mail@stravid.com>
2012-09-07 19:39:25 +0200

    Store asset size directly on Document model for later use

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,8 +1,15 @@
 class Document < ActiveRecord::Base
   belongs_to :uploader, :class_name => 'User', :foreign_key => :user_id
 
+  before_save :save_size
+
   mount_uploader :asset, AssetUploader
   attr_accessible :title, :asset, :asset_cache
 
   validates :title, :asset, :presence => true
+
+  private
+  def save_size
+    self.size = asset.file.size
+  end
 end
