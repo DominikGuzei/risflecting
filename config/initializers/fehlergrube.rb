beee903780fdd527395d8aca4de9c583b2aeff65
David Strauß <mail@stravid.com>
2012-09-12 13:00:49 +0200

    Move Fehlergrube credentials to configuration file

--- config/initializers/fehlergrube.rb
+++ config/initializers/fehlergrube.rb
@@ -1,6 +1,6 @@
 Airbrake.configure do |config|
-   config.api_key     = 'af0ca3e11eae1271d0cd7e2868f07803'
-   config.host        = 'fehlergrube.herokuapp.com'
+   config.api_key     = ENV['FEHLERGRUBE_API_KEY']
+   config.host        = ENV['FEHLERGRUBE_HOST']
    config.port        = 80
    config.secure      = config.port == 443
 end
