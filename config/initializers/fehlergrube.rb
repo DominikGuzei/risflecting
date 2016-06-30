73d465a68da1d0d28280576e80b78d38fe5c7040
David Strauß <david@strauss.io>
2016-06-30 11:12:53 +0200

    Remove Fehlergrube initializer

--- config/initializers/fehlergrube.rb
+++
@@ -1,6 +0,0 @@
-Airbrake.configure do |config|
-   config.api_key     = ENV['FEHLERGRUBE_API_KEY']
-   config.host        = ENV['FEHLERGRUBE_HOST']
-   config.port        = 80
-   config.secure      = config.port == 443
-end
